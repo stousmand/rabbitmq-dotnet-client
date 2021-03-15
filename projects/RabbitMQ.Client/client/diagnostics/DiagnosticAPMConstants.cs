bbee05924f457ef4b262b49e8fc5b418a6cb6d8d
stousmand <shirhan.apache@gmail.com>
2021-03-15 13:21:46 +0530

    Adds initial classes necessary for Diagnostics
    Adds initial classes necessary for Diagnostics capturing

---
+++ projects/RabbitMQ.Client/client/diagnostics/DiagnosticAPMConstants.cs
@@ -0,0 +1,25 @@
+﻿/*
+ *  1.0.0               20210315            shirhan.apache@gmail.com                Certain constants that are used to define events
+ *                                                                                  to be identified by the tracing instrumentation (in Elastic APM)
+ *                                                                                  Diagnostic source is implemented in RabbitMq client classes.
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RabbitMQ.Client.Diagnostics
+{
+    public enum DiagnosticAPMConstants
+    {
+        EXCEPTION = 0,
+        CREATE_CONNECTION_START = 11,
+        CREATE_CONNECTION_END = 12,
+        BASIC_PUBLISH_START = 13,
+        BASIC_PUBLISH_END = 14
+    }
+
+    public static class DiagnosticConstants
+    {
+        public static string DIAGNOSTIC_SOURCE_NAME = "RabbitMQ.Client.DiagnosticListener";
+    }
+}
